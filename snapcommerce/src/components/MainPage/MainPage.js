249a066de0197123f1fca584a198ef408f4314d3
tuananhlehuy1111 <tuananhlehuy1111@gmail.com>
2023-12-31 09:10:06 +0700

    first commit

--- snapcommerce/src/components/MainPage/MainPage.js
+++
@@ -1,4 +0,0 @@
-function MainPage() {
-    return <h2>Main page passed!!</h2>;
-}
-export default MainPage;
--- snapcommerce/src/components/ProductPage/ProductPage.js
+++
@@ -1,4 +0,0 @@
-function ProductPage() {
-    return <h2>Product page passed!!!</h2>;
-}
-export default ProductPage;
